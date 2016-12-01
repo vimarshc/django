564a8c490bedf51b8f7e18356b37f56085b740a4
vimarshc <vimarsh.chaturvedi@hedgehoglab.com>
2016-12-01 20:09:31 +0530

    Added test cases for nested prefetches failing.

--- tests/flattened-prefetch/models.py
+++ tests/flattened-prefetch/models.py
@@ -1,16 +1,19 @@
 from django.db import models
 
 
+
 class A(models.Model):
     pass
 
+
 class B(models.Model):
-    a = models.ForeignKey(A)
+    a = models.OneToOneField('A', null=True, related_name='b', on_delete=models.CASCADE)
     d = models.ManyToManyField('D', through='C', related_name='b')
 
+
 class C(models.Model):
-    b = models.ForeignKey('B', related_name='c')
-    d = models.ForeignKey('D', related_name='c')
+    b = models.ForeignKey('B', related_name='c', on_delete=models.CASCADE)
+    d = models.ForeignKey('D', related_name='c', on_delete=models.CASCADE)
 
 
 class D(models.Model):
--- tests/flattened-prefetch/tests.py
+++ tests/flattened-prefetch/tests.py
@@ -11,35 +11,59 @@ from .models import A,B,C,D
 
 class Prefetch_Test_Case(TestCase):
 
-    def test_case_for_nested_prefetches(self):
+    def test_case_for_only_nested_prefetches(self):
         a = A.objects.create()
         b = B.objects.create(a=a)
         d = D.objects.create()
         C.objects.create(b=b, d=d)
-        #
-        # queryset = A.objects.all().prefetch_related(
-        #     Prefetch(
-        #         'b',
-        #         queryset=B.objects.all().prefetch_related(
-        #             Prefetch(
-        #                 'c',
-        #                 queryset=C.objects.all().prefetch_related(
-        #                     'd'
-        #                 )
-        #             )
-        #         )
-        #     )
-        # )
 
-        # queryset = A.objects.all().prefetch_related(Prefetch('b'))
-        # queryset = A.objects.all().prefetch_related(
-        #     Prefetch('b__c__d')
-        # )
-        # content = queryset[0].b.c.all()[0].d.pk
-        # print (content)
-        if a and b and d:
-            content = 1
-        else:
-            content = 9
+        queryset = A.objects.all().prefetch_related(
+            Prefetch(
+                'b',
+                queryset=B.objects.all().prefetch_related(
+                    Prefetch(
+                        'c',
+                        queryset=C.objects.all().prefetch_related(
+                            'd'
+                        )
+                    )
+                )
+            )
+        )
+        content = queryset[0].b.c.all()[0].d.pk
+
+
         self.assertEqual(content, 1)
 
+
+    def test_case_for_nested_and_flat_prefetches(self):
+        a = A.objects.create()
+        b = B.objects.create(a=a)
+        d = D.objects.create()
+        C.objects.create(b=b, d=d)
+
+        queryset = A.objects.all().prefetch_related(
+            Prefetch(
+                'b',
+                queryset=B.objects.all().prefetch_related(
+                    Prefetch('c__d')
+                )
+            )
+        )
+        content = queryset[0].b.c.all()[0].d.pk
+
+        self.assertEqual(content, 1)
+
+
+    def test_case_for_only_flat_prefetches(self):
+        a = A.objects.create()
+        b = B.objects.create(a=a)
+        d = D.objects.create()
+        C.objects.create(b=b, d=d)
+
+        queryset = A.objects.all().prefetch_related(
+            Prefetch('b__c__d')
+        )
+        content = queryset[0].b.c.all()[0].d.pk
+
+        self.assertEqual(content, 1)
